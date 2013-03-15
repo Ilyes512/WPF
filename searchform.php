3086bf9d4f5666ead553d578d26e7a939a1d86b0
MekZii <ilyes.ahidar@gmail.com>
2013-03-15 02:23:15 +0100

    Revert "Updated version in readme"
    This reverts commit cace5a1b25ba644bf7e61c1a54165dda638704b6.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <!-- searchform.php -->
-<form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
+<form <?php if (isset($append_class_to_searchform)) echo 'class="'.$append_class_to_searchform.'"'; ?> role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
 	<div class="row collapse">
 		<div class="small-8 columns">
 			<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'wpf'); ?>">
