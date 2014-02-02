4ae9f70115630c7832f87a2a13d34ea6a8c41556
Ilyes512 <ilyes.ahidar@gmail.com>
2014-02-02 22:10:01 +0100

    Changed dummy text in contextual-help.inc.php

--- inc/wpf/admin/contextual-help.inc.php
+++ inc/wpf/admin/contextual-help.inc.php
@@ -16,17 +16,21 @@ if ( ! function_exists( 'wpf_options_help_tab' ) ) {
 
 		switch ( $GLOBALS['active_tab'] ) {
 			case 'general':
-				$screen->add_help_tab( array(
-					'id'         => 'wpf-help-general-layout',
-					'title'      => __( 'Layout Options', 'wpf' ),
-					'content'    => __( '<h5>Testing</h5><p>This is some general input<p>' , 'wpf' )
-				) );
+				$screen->add_help_tab(
+					array(
+						'id'         => 'wpf-help-general-layout',
+						'title'      => __( 'Layout Options', 'wpf' ),
+						'content'    => __( '<h5>Testing</h5><p>This is some dummy input<p>', 'wpf' ),
+					)
+				);
 
-				$screen->add_help_tab( array(
-					'id'         => 'wpf-help-general-post',
-					'title'      => __( 'Post Options', 'wpf' ),
-					'content'    => __( '<p>This is some general input</p>' , 'wpf' )
-				) );
+				$screen->add_help_tab(
+					array(
+						'id'         => 'wpf-help-general-post',
+						'title'      => __( 'Post Options', 'wpf' ),
+						'content'    => __( '<p>This is some dummy input</p>', 'wpf' ),
+					)
+				);
 				break;
 
 			case 'header':
